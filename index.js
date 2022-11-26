698a77e435151174e049b34232232a868b27794f
Josphat-Kip <josphatkiplimo247@gmail.com>
2022-11-26 13:12:55 +0300

    Jose

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-function scuberGreetingForFeet(){
-  function scuberGreetingForFeet(distance){
-    // Write your code here!
-    if (distance <=400)
-    {
-      return `This one is on me!`;
-    }else if(distance <= 2499 )
-    {
-      return `I will gladly take your thirty bucks.`;
-    }else
-    {
-      return `No can do.`;
-    }
-  }
-  
-  function ternaryCheckCity(){
-  function ternaryCheckCity(city){
-    // Write your code here!
-    return  city === 'NYC' ?  "Ok, sounds good." : "No go.";
-  }
-  
-  function switchOnCharmFromTip(){
-  function switchOnCharmFromTip(tip){
-    // Write your code here!
-    switch (tip){
-      case 'generous' :
-        return `Thank you so much.`;
-        break;
-        case 'not as generous' :
-          return `Thank you.`;
-          break;
-          case `thanks for everything`:
-            return `Bye.`;
-    }
-  }
+function scuberGreetingForFeet(distance){
+if ((distance) <=400){
+  return ('This one is on me!');
+}
+else if ((distance)>2000 && (distance)<=2500){
+  return ("I will gladly take your thirty bucks.");
+}else {
+  return 'No can do.'
+}
+}
+function ternaryCheckCity(city){
+
+  return (city === "NYC")? "Ok, sounds good."  : "No go."
+}
+
+console.log(switchOnCharmFromTip(" not generous"));
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' :
+      return `Thank you so much.`;
+      case 'not as generous':
+        return `Thank you.`;
+        case `thanks for everything`:
+          return `Bye.`;
+  }}
